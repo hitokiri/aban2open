237edbb64f115b3b66373e36a36059b235ad013e
hiko_hitokiri <lastvnm@gmail.com>
2014-04-20 17:03:33 -0600

    agregado template

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ from procesar_csv_dict import ProcesarCsv
 if __name__ =="__main__":
 	obj = ProcesarCsv()
 	obj.escribir_datos_product()
+	obj.escribir_datos_template()
--- procesar_csv_dict.py
+++ procesar_csv_dict.py
@@ -33,7 +33,24 @@ class ProcesarCsv(object):
 		 			nuevo_product.writerow(valores_plantilla)
 		 			count += 1
 		 		break
-	def escribir_datos_template():
-		pass
+
+	def escribir_datos_template(self):
+		count = 4
+		aban = self.datos_csv(self.datos_csv_aban, 'r')
+		product = self.datos_csv(self.datos_product_template, 'r')
+		nombre_orden = product.fieldnames
+		orden_aban = aban.fieldnames
+		with open('db_files/product_template1.csv', 'wb') as nuevo:
+		 	for master, plantilla in zip(aban, product):
+		 		nuevo_product = csv.DictWriter(nuevo,quotechar='"', quoting=csv.QUOTE_ALL, fieldnames = nombre_orden)
+		 		nuevo_product.writerow(dict((fn,fn) for fn in nombre_orden))
+		 		valores_plantilla = plantilla
+		 		for valor_master in  aban:
+		 			valores_plantilla.update({'id':'%s' % count})
+		 			valores_plantilla.update({'list_price':'%s' % valor_master['pvp']})
+		 			valores_plantilla.update({'name':'%s' % valor_master['referencia']})
+		 			nuevo_product.writerow(valores_plantilla)
+		 			count += 1
+		 		break
 
 
