5c31d38b6ec96ca00bbef7f3bdbff87352e7ca15
hiko_hitokiri <lastvnm@gmail.com>
2014-04-27 15:10:23 -0600

    mejora de product_template, y creacion de product_category

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ from procesar_csv_dict import ProcesarCsv
 
 if __name__ =="__main__":
 	obj = ProcesarCsv()
-	obj.escribir_datos_product()
+	# obj.escribir_datos_product()
 	obj.escribir_datos_template()
+	# obj.escribir_datos_category()
--- procesar_csv_dict.py
+++ procesar_csv_dict.py
@@ -8,6 +8,7 @@ class ProcesarCsv(object):
 		self.datos_csv_aban = 'db_files/db-colum.csv'
 		self.datos_prodct_product = 'db_files/product_product.csv'
 		self.datos_product_template = 'db_files/product_template.csv'
+		self.datos_product_category = 'db_files/product_category1.csv'
 
 	def datos_csv(self, nombre, modo):
 		datos_csv=open(nombre, modo)
@@ -38,19 +39,45 @@ class ProcesarCsv(object):
 		count = 4
 		aban = self.datos_csv(self.datos_csv_aban, 'r')
 		product = self.datos_csv(self.datos_product_template, 'r')
+		product_category = self.datos_csv(self.datos_product_category, 'r')
 		nombre_orden = product.fieldnames
 		orden_aban = aban.fieldnames
 		with open('db_files/product_template1.csv', 'wb') as nuevo:
 		 	for master, plantilla in zip(aban, product):
 		 		nuevo_product = csv.DictWriter(nuevo,quotechar='"', quoting=csv.QUOTE_ALL, fieldnames = nombre_orden)
-		 		nuevo_product.writerow(dict((fn,fn) for fn in nombre_orden))
+		 		nuevo_product.writerow(dict((_, _) for _ in nombre_orden))
 		 		valores_plantilla = plantilla
 		 		for valor_master in  aban:
 		 			valores_plantilla.update({'id':'%s' % count})
 		 			valores_plantilla.update({'list_price':'%s' % valor_master['pvp']})
 		 			valores_plantilla.update({'name':'%s' % valor_master['referencia']})
+		 			for category in product_category:
+		 				if valor_master['codfamilia'] == category['name']:
+		 					valores_plantilla.update({'categ_id':'%s' % category['id']})
 		 			nuevo_product.writerow(valores_plantilla)
+		 			product_category = self.datos_csv(self.datos_product_category, 'r')
 		 			count += 1
+		 		break
+
+	def escribir_datos_category(self):
+		count = 3
+		aban = self.datos_csv(self.datos_csv_aban, 'r')
+		product = self.datos_csv(self.datos_product_category, 'r')
+		nombre_orden = product.fieldnames
+		orden_aban = aban.fieldnamesvalores_plantilla.update({'id':'%s' % count})
+		familias = [_['codfamilia'] for _ in aban]
+		familias_category = set(familias)
+		with open('db_files/product_category1.csv', 'wb') as nuevo:
+		 	for plantilla in product:
+		 		nuevo_product = csv.DictWriter(nuevo,quotechar='"', quoting=csv.QUOTE_ALL, fieldnames = nombre_orden)
+		 		nuevo_product.writerow(dict((_, _) for _ in nombre_orden))
+		 		valores_plantilla = plantilla
+		 		for valor_master in  familias_category:
+		 			if valor_master:
+			 			valores_plantilla.update({'id':'%s' % count})
+			 			valores_plantilla.update({'name':'%s' % valor_master})
+			 			nuevo_product.writerow(valores_plantilla)
+			 			count += 1
 		 		break
 
 
